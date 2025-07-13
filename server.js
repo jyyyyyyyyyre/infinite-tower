b030ae55546217ab3911a918830d386538cd6e6b
jyyyyyyyyyre <algml7913@gmail.com>
2025-07-13 13:26:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ const PORT = 3000;
 const TICK_RATE = 1000; 
 const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
-const ADMIN_OBJECT_ID = '6873333f38f1aa40c838b668';
+const ADMIN_OBJECT_ID = '6873333f38f1aa40c838b666';
 const BOSS_INTERVAL = 200;
 
 
